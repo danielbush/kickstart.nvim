3483c13061e1d7fe64550393cce22d1c54c33bb1
Daniel Bush <dlb.id.au@gmail.com>
2025-08-03 11:33:57 +1000

    chore: various (markdown)

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -3,7 +3,7 @@ local search_utils = require 'search_utils'
 vim.api.nvim_buf_set_keymap(0, 'n', '<localleader>jf', '/(fun)<CR>zv', { noremap = true, silent = true, desc = '(fun)' })
 -- jump headings
 vim.api.nvim_buf_set_keymap(0, 'n', '<localleader>jh', '/^#.*<CR>zv', { noremap = true, silent = true, desc = 'Jump headings' })
-vim.api.nvim_buf_set_keymap(0, 'n', '<localleader>jt', '/\\<TARGETS<CR>zv', { noremap = true, silent = true, desc = 'Jump targets' })
+vim.api.nvim_buf_set_keymap(0, 'n', '<localleader>jt', '/^r:TARGETS<CR>zv', { noremap = true, silent = true, desc = 'Jump targets' })
 -- collapse level 3+
 vim.api.nvim_buf_set_keymap(0, 'n', '<localleader>,', 'zmzr', { noremap = true, silent = true, desc = 'Collapse level3+' })
 -- jump hats
@@ -39,6 +39,7 @@ local function fuzzy_search_i_identifiers()
   search_utils.fuzzy_search_pattern('i:[A-Z0-9_]+', 'i: identifiers', 'i: Identifiers', false)
 end
 vim.keymap.set('n', '<localleader>fi', fuzzy_search_i_identifiers, { desc = 'Find i: identifiers' })
+vim.keymap.set('n', '<localleader>i', fuzzy_search_i_identifiers, { desc = 'Find i: identifiers' })
 
 local function fuzzy_search_ir_identifiers()
   search_utils.fuzzy_search_pattern('[ir]:[A-Z0-9_]+', 'i/r: identifiers', 'i/r: Identifiers', true)
@@ -49,6 +50,7 @@ local function fuzzy_search_headings()
   search_utils.fuzzy_search_pattern('^#.*$', 'Headings', 'Headings', false)
 end
 vim.keymap.set('n', '<localleader>fh', fuzzy_search_headings, { desc = 'Search headings' })
+vim.keymap.set('n', '<localleader>h', fuzzy_search_headings, { desc = 'Search headings' })
 -- vim.keymap.set('n', '<leader>fp', function()
 --   vim.ui.input({ prompt = 'Pattern: ' }, function(pattern)
 --     if pattern then fuzzy_search_pattern(pattern, "matches", "Pattern Matches") end
